036e007b721aca95377d0913a692492e37b89c34
daotung <daotungsl@gmail.com>
2019-06-06 17:32:16 +0700

    aa

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/angular-news'));
+app.use(express.static(__dirname + '/dist/angular-news-eagle-2019'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/angular-news/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-news-eagle-2019/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080, function(){
