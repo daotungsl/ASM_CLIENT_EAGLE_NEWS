c6a30a240c611b77aaa5ff436654cc368ac234d1
daotung <daotungsl@gmail.com>
2019-06-06 16:53:28 +0700

    aa

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/angular-news'));
+app.use(express.static(__dirname + '/dist'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/angular-news/index.html'));
+  res.sendFile(path.join(__dirname + '/dist'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
